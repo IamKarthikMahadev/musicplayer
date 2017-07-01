69078b95c305da7a1edba78ff56bfeccefefcf06
Vidhya Karthikeyan <vidhyakarthikeyan@Vidhyas-MacBook-Pro.local>
2017-07-01 10:32:45 -0700

    Using queues

--- playerproxy/playerproxy/src/MusicPlayerHandler.cpp
+++ playerproxy/playerproxy/src/MusicPlayerHandler.cpp
@@ -1,111 +1,69 @@
-//#include "Socket.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+
 
 #include "Settings.h"
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
-
-//int main(int argc, char *argv[])
-//{
-//    int sockfd, newsockfd, portno;
-//    socklen_t clilen;
-//    char buffer[256];
-//    struct sockaddr_in serv_addr, cli_addr;
-//    int n;
-//    if (argc < 2) {
-//        fprintf(stderr,"ERROR, no port provided\n");
-//        exit(1);
-//    }
-//    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//    if (sockfd < 0)
-//        error("ERROR opening socket");
-//    bzero((char *) &serv_addr, sizeof(serv_addr));
-//    portno = atoi(argv[1]);
-//    serv_addr.sin_family = AF_INET;
-//    serv_addr.sin_addr.s_addr = INADDR_ANY;
-//    serv_addr.sin_port = htons(portno);
-//    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-//             sizeof(serv_addr)) < 0)
-//        error("ERROR on binding");
-//    listen(sockfd,5);
-//    clilen = sizeof(cli_addr);
-//    printf("Before accept\n");
-//    newsockfd = accept(sockfd,
-//                       (struct sockaddr *) &cli_addr,
-//                       &clilen);
-//    printf("After accept\n");
-//    if (newsockfd < 0)
-//        error("ERROR on accept");
-//    bzero(buffer,256);
-//    printf("Before read\n");
-//    n = read(newsockfd,buffer,255);
-//    printf("After read\n");
-//    if (n < 0) error("ERROR reading from socket");
-//    printf("Here is the message: %s\n",buffer);
-//    printf("Before write\n");
-//    n = write(newsockfd,"I got your message",18);
-//    printf("After write\n");
-//    if (n < 0) error("ERROR writing to socket");
-//    close(newsockfd);
-//    close(sockfd);
-//    return 0;
-//}
 #include "MusicPlayerHandler.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MSGSZ     128
+
+
+/*
+ * Declare the message structure.
+ */
+
+typedef struct msgbuf {
+    long    mtype;
+    char    mtext[MSGSZ];
+} message_buf;
+
 namespace MusicPlayer
 {
     void* MusicPlayerHandlerThread(void* intArg)
     {
-        int sockfd, newsockfd, portno;
-        socklen_t clilen;
-        char buffer[256];
-        struct sockaddr_in serv_addr, cli_addr;
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0)
-            error("ERROR opening socket");
-        bzero((char *) &serv_addr, sizeof(serv_addr));
+        int msqid;
+        key_t key;
+        message_buf  rbuf;
         
-        portno = port_number;
+        /*
+         * Get the message queue id for the
+         * "name" 1234, which was created by
+         * the server.
+         */
+        key = 1234;
         
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        serv_addr.sin_port = htons(portno);
-        if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                 sizeof(serv_addr)) < 0)
-            error("ERROR on binding");
+        do {
 
-        while(1)
-        {
-            listen(sockfd,5);
-            clilen = sizeof(cli_addr);
-            newsockfd = accept(sockfd,
-                               (struct sockaddr *) &cli_addr,
-                               &clilen);
-            if (newsockfd < 0)
-                error("ERROR on accept");
-            bzero(buffer,256);
-            int n = read(newsockfd,buffer,255);
-            if (n < 0) error("ERROR reading from socket");
-            if(n>0)
-            {
-                
-                printf("Here is the message: %s\n",buffer);
-                n = write(newsockfd,"I got your message",18);
-                if (n < 0) error("ERROR writing to socket");
+            
+            if ((msqid = msgget(key, 0666)) < 0) {
+                perror("msgget");
+                return 0;
             }
-            close(newsockfd);
-            close(sockfd);
-        }
+            
+            
+            /*
+             * Receive an answer of message type 1.
+             */
+            if (msgrcv(msqid, &rbuf, MSGSZ, 1, 0) < 0) {
+                perror("msgrcv");
+                return 0;
+            }
+            
+            /*
+             * Print the answer.
+             */
+            printf("Got %s\n", rbuf.mtext);
+            //return 0;
+            
+
+        } while (1);
+        
+       
         return 0;
         
     }
--- playerproxy/playerproxy/src/Settings.h
+++ playerproxy/playerproxy/src/Settings.h
@@ -1,5 +1,3 @@
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
-const int port_number = 8880;
-
 #endif //SETTINGS_H_
